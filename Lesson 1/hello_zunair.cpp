b1c04593581e8bdf68cc5f84dbdabed3fa77e4e0
zunairaslam <zunairaslam36@gmail.com>
2021-12-09 11:35:33 -0800

    sum print

---
+++ Lesson 1/hello_zunair.cpp
@@ -0,0 +1,18 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main() {
+   int amount1;
+   cout << "Hello World! \n";
+  cout << "I am learning C++\n";
+   cout<<"Please enter first number"<<endl;
+   cin>>amount1;
+
+   int amount2;
+   cout<<"Please enter second number\n";
+   cin>>amount2;
+
+   int sum=amount1+amount2;
+   cout<<"answer: ";
+   cout<<sum<<endl;
+}
