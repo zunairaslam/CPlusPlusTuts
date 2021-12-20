ea2040c4a9bac5ba4b50622e907f787c910026a5
zunairaslam <zunairaslam36@gmail.com>
2021-12-20 14:04:57 -0800

    loop_lesson

---
+++ lesson4/while_loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n;
+ cin>>n;
+
+ int sum=0;
+
+ for (int counter = 1; counter <=n; counter++)
+ {
+     cout<<"counter value\n";
+     cout<<counter<<endl;
+     sum=sum+counter;
+     cout<<"sum value\n";
+     cout<< sum<<endl;
+ }
+ 
+
+}
+
+
---
+++ lesson4/while_loop2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"please enter the value of n";
+    cin>>n;
+
+    while (n>0)
+    {
+    
+        cout<<n<<endl;
+        cout<<"please enter the value of n";
+        cin>>n;
+
+    }
+    
+    
+}
